7edd84b4d30543cd2ccd86c278e88eab75949454
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-05-29 10:31:56 -0600

    Added new function and deleted unnecessary files

--- Sources/SwiftPackage/SwiftPackage.swift
+++ Sources/SwiftPackage/SwiftPackage.swift
@@ -3,7 +3,17 @@ public struct SwiftPackage {
     public init() {
     }
     
+    
+    
+    /// A function that prints "Hello World"
     public static func helloWorld() {
         print("Hello World 🌎")
+    }
+    
+    /// Returns a string based off the parameters
+    /// - Parameter text: String input to output
+    /// - Returns: String output from input
+    public static func returnString(text: String) -> String {
+        return text
     }
 }
